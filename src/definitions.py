078f0247b8661290b61599477a8af118909a7544
James Ross <james.rosstwo@gmail.com>
2024-04-09 22:10:56 -0700

    add gitignore, data parsing

---
+++ src/definitions.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_PATH = Path(SRC_DIR)
+ROOT_PATH = SRC_PATH.parent.resolve()
+DATA_PATH = ROOT_PATH / "data"
+DATA_PATH.mkdir(exist_ok=True)
+EXPERIMENTS_PATH = ROOT_PATH / "experiments"
+EXPERIMENTS_PATH.mkdir(exist_ok=True)

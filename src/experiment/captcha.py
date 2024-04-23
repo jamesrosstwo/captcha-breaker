249f9a005dbd376e234ef04a44b5321ab30a2263
Al20vortex <Al20vortex@gmail.com>
2024-04-23 15:32:56 -0700

    Added weight freezing to text and image backbones, and added some basic metric prints to training and val

--- src/experiment/captcha.py
+++ src/experiment/captcha.py
@@ -42,6 +42,10 @@ class CaptchaExperiment:
 
     def _train_epoch(self, epoch: int):
         loader = self._train_dataset.construct_loader(**self._loader_args)
+        total_loss = 0
+        total_accuracy = 0
+        total_samples = 0
+
         for batch_idx, (questions, challenges, selections) in tqdm(enumerate(loader)):
             challenges = challenges.to(device)
             selections = selections.to(device)
@@ -52,16 +56,54 @@ class CaptchaExperiment:
             self._optimizer.step()
             self._optimizer.zero_grad()
 
+            total_loss += loss.item() * selections.size(0)
+            total_accuracy += ((preds.round() == selections).sum().item())
+            total_samples += selections.size(0)
+
+        avg_loss = total_loss / total_samples
+        avg_accuracy = total_accuracy / total_samples
+
+        print(f"Epoch {epoch+1}/{self._train_epochs}, Train Loss: {avg_loss:.4f}, Train Accuracy: {avg_accuracy:.4f}")
+
+        return avg_loss, avg_accuracy
+
         # TODO: Return some kind of aggregated loss
         # Or upload directly to wandb?
 
     def _train_architecture(self):
         for epoch in range(self._train_epochs):
             print("Training epoch {}".format(epoch))
-            self._train_epoch(epoch)
+            train_loss, train_accuracy = self._train_epoch(epoch)
+            
+            # TODO: Log train_loss and train_accuracy to wandb
+            val_loss, val_accuracy = self._evaluate_architecture()
+
 
     def _evaluate_architecture(self):
-        pass
+        loader = self._val_dataset.construct_loader(**self._loader_args)
+        total_loss = 0
+        total_accuracy = 0
+        total_samples = 0
+
+        self._architecture.eval()
+        with torch.no_grad():
+            for batch_idx, (questions, challenges, selections) in tqdm(enumerate(loader)):
+                challenges = challenges.to(device)
+                selections = selections.to(device)
+
+                preds = self._architecture(questions, challenges)
+                loss = self._loss_fn(preds, selections)
+
+                total_loss += loss.item() * selections.size(0)
+                total_accuracy += ((preds.round() == selections).sum().item())
+                total_samples += selections.size(0)
+
+        avg_loss = total_loss / total_samples
+        avg_accuracy = total_accuracy / total_samples
+
+        print(f"Validation Loss: {avg_loss:.4f}, Validation Accuracy: {avg_accuracy:.4f}")
+
+        return avg_loss, avg_accuracy
 
     def run(self):
         self._train_architecture()
